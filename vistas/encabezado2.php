64970dbc1a47e3fe24c1afb24f0a1c8ba8e77dec
esteban1234 <tva_libra@hotmail.com>
2017-11-28 18:12:31 -0600

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="_idCbFekT7g8LgqptB9PUDKAwKqSe6lfLw54VjQLDjY" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>PUBLICIDAD ZENS</title>
 	 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
